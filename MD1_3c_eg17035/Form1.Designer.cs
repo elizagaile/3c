e9295372357ad71f3a27ad4175e1dfc55d7a9a09
unknown <eliizagaile@gmail.com>
2019-10-19 17:16:44 +0300

    Main Algorithm

---
+++ MD1_3c_eg17035/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MD1_3c_eg17035
+{
+    partial class OffsetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canva = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbLineStart = new System.Windows.Forms.Label();
+            this.lbLineEnd = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbQuad1 = new System.Windows.Forms.Label();
+            this.lbQuad2 = new System.Windows.Forms.Label();
+            this.lbQuad3 = new System.Windows.Forms.Label();
+            this.lbQuad4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Canva)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canva
+            // 
+            this.Canva.BackColor = System.Drawing.Color.White;
+            this.Canva.Location = new System.Drawing.Point(13, 13);
+            this.Canva.Name = "Canva";
+            this.Canva.Size = new System.Drawing.Size(1000, 1000);
+            this.Canva.TabIndex = 0;
+            this.Canva.TabStop = false;
+            this.Canva.Paint += new System.Windows.Forms.PaintEventHandler(this.Canva_Paint);
+            this.Canva.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canva_MouseDown);
+            this.Canva.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canva_MouseMove);
+            this.Canva.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canva_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1019, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nogriežņa galapunktu koordinātas:";
+            // 
+            // lbLineStart
+            // 
+            this.lbLineStart.AutoSize = true;
+            this.lbLineStart.Location = new System.Drawing.Point(1020, 37);
+            this.lbLineStart.Name = "lbLineStart";
+            this.lbLineStart.Size = new System.Drawing.Size(23, 20);
+            this.lbLineStart.TabIndex = 5;
+            this.lbLineStart.Text = "(;)";
+            // 
+            // lbLineEnd
+            // 
+            this.lbLineEnd.AutoSize = true;
+            this.lbLineEnd.Location = new System.Drawing.Point(1020, 57);
+            this.lbLineEnd.Name = "lbLineEnd";
+            this.lbLineEnd.Size = new System.Drawing.Size(23, 20);
+            this.lbLineEnd.TabIndex = 6;
+            this.lbLineEnd.Text = "(;)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1020, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Četrstūra virsotņu koordinātas:";
+            // 
+            // lbQuad1
+            // 
+            this.lbQuad1.AutoSize = true;
+            this.lbQuad1.Location = new System.Drawing.Point(1020, 141);
+            this.lbQuad1.Name = "lbQuad1";
+            this.lbQuad1.Size = new System.Drawing.Size(23, 20);
+            this.lbQuad1.TabIndex = 8;
+            this.lbQuad1.Text = "(;)";
+            // 
+            // lbQuad2
+            // 
+            this.lbQuad2.AutoSize = true;
+            this.lbQuad2.Location = new System.Drawing.Point(1020, 161);
+            this.lbQuad2.Name = "lbQuad2";
+            this.lbQuad2.Size = new System.Drawing.Size(23, 20);
+            this.lbQuad2.TabIndex = 9;
+            this.lbQuad2.Text = "(;)";
+            // 
+            // lbQuad3
+            // 
+            this.lbQuad3.AutoSize = true;
+            this.lbQuad3.Location = new System.Drawing.Point(1020, 181);
+            this.lbQuad3.Name = "lbQuad3";
+            this.lbQuad3.Size = new System.Drawing.Size(23, 20);
+            this.lbQuad3.TabIndex = 10;
+            this.lbQuad3.Text = "(;)";
+            // 
+            // lbQuad4
+            // 
+            this.lbQuad4.AutoSize = true;
+            this.lbQuad4.Location = new System.Drawing.Point(1020, 201);
+            this.lbQuad4.Name = "lbQuad4";
+            this.lbQuad4.Size = new System.Drawing.Size(23, 20);
+            this.lbQuad4.TabIndex = 11;
+            this.lbQuad4.Text = "(;)";
+            // 
+            // OffsetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1343, 1024);
+            this.Controls.Add(this.lbQuad4);
+            this.Controls.Add(this.lbQuad3);
+            this.Controls.Add(this.lbQuad2);
+            this.Controls.Add(this.lbQuad1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbLineEnd);
+            this.Controls.Add(this.lbLineStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Canva);
+            this.Name = "OffsetForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Canva)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Canva;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbLineStart;
+        private System.Windows.Forms.Label lbLineEnd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbQuad1;
+        private System.Windows.Forms.Label lbQuad2;
+        private System.Windows.Forms.Label lbQuad3;
+        private System.Windows.Forms.Label lbQuad4;
+    }
+}
+
---
+++ MD1_3c_eg17035/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MD1_3c_eg17035
+{
+    // Izstrādāt datorprogrammu, kas realizē taisnes nogriežņa un patvaļīga slīpuma taisnstūra
+    // parametrisko apcirpšanu.
+
+
+    public partial class OffsetForm : Form
+    {
+        Point moving = new Point();
+
+        private PointF[] P = new PointF[4];
+        PointF start = new PointF();
+        PointF end = new PointF();
+
+        public OffsetForm()
+        {
+            InitializeComponent();
+
+            P[0] = new PointF(50, 100);
+            P[1] = new PointF(300, 100);
+            P[2] = new PointF(300, 200);
+            P[3] = new PointF(50, 200);
+
+            start = new PointF(10, 10);
+            end = new PointF(300, 300);
+
+            moving = new Point(-1, -1);
+        }
+
+        private void Canva_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; // grafiski attēlo līnijas gludākas
+
+            e.Graphics.DrawLines(Pens.Black, P);
+            e.Graphics.DrawLine(Pens.Black, P[3], P[0]);
+
+            e.Graphics.FillEllipse(Brushes.Black, start.X - 2, start.Y - 2, 2 * 2, 2 * 2);
+            e.Graphics.FillEllipse(Brushes.Black, end.X - 2, end.Y - 2, 2 * 2, 2 * 2);
+
+            PointF realStart = GetPointFromT(GetRealCrossPoint(0));
+            PointF realEnd = GetPointFromT(GetRealCrossPoint(1));
+
+            e.Graphics.DrawLine(Pens.Blue, realStart, realEnd);
+
+            lbLineStart.Text = "P1: (" + start.X + ", " + start.Y + ")";
+            lbLineEnd.Text = "P1: (" + end.X + ", " + end.Y + ")";
+
+            lbQuad1.Text = "P1: (" + P[0].X + ", " + P[0].Y + ")";
+            lbQuad2.Text = "P2: (" + P[1].X + ", " + P[1].Y + ")";
+            lbQuad3.Text = "P3: (" + P[2].X + ", " + P[2].Y + ")";
+            lbQuad4.Text = "P4: (" + P[3].X + ", " + P[3].Y + ")";
+        }
+
+        private double GetRealCrossPoint(int type)
+        {
+            List<double> tIe = new List<double>();
+            List<double> tIz = new List<double>();
+
+            PointF lineVector = new PointF
+            {
+                X = end.X - start.X,
+                Y = end.Y - start.Y
+            };
+
+            for (int i = 0; i < 4; i++)
+            {
+                PointF normal = new PointF
+                {
+                    X = P[i].Y - P[(i + 1) % 4].Y,
+                    Y = P[(i + 1) % 4].X - P[i].X
+                };
+
+                PointF startToP = new PointF
+                {
+                    X = P[i].X - start.X,
+                    Y = P[i].Y - start.Y
+                };
+
+
+                double num = DotProduct(normal, startToP);
+                double den = DotProduct(normal, lineVector);
+
+                if (den > 0)
+                {
+                    tIe.Add(num / den);
+                }
+                else if (den < 0)
+                {
+                    tIz.Add(num / den);
+                }
+            }
+
+            tIe.Add(0);
+            tIe.Sort();
+            tIz.Add(1);
+            tIz.Sort();
+
+            double tStart = tIe[tIe.Count - 1];
+            double tEnd = tIz[0];
+
+            if(tStart > tEnd)
+            {
+                return -1;
+            }
+
+            else if (type == 0)
+            {
+                return tStart;
+            }
+
+            else
+            {
+                return tEnd;
+            }
+        }
+
+        private double DotProduct(PointF a, PointF b)
+        {
+            return (a.X * b.X + a.Y * b.Y);
+        }
+
+        private PointF GetPointFromT(double t)
+        {
+            double x = start.X + (end.X - start.X) * t;
+            double y = start.Y + (end.Y - start.Y) * t;
+
+            return new PointF((float)x, (float)y);
+        }
+
+        private Point FindLocalPoint(PointF mouseLocation)
+        {
+            const int localRadius = 7; // apkārtnes rādiusa izmērs pikseļos
+
+            if (GetLength(mouseLocation, start) < localRadius)
+            {
+                return new Point(0, 0);
+            }
+
+            if (GetLength(mouseLocation, end) < localRadius)
+            {
+                return new Point(0, 1);
+            }
+
+            for (int i = 0; i < P.Length; i++)
+            {
+                if (GetLength(mouseLocation, P[i]) < localRadius)
+                {
+                    return new Point(1, i);
+                }
+            }
+            return new Point(-1, -1);
+        }
+
+        private float GetLength(PointF firstPoint, PointF secondPoint)
+        {
+            return (float)Math.Sqrt(Math.Pow(firstPoint.X - secondPoint.X, 2) + Math.Pow(firstPoint.Y - secondPoint.Y, 2));
+        }
+
+        private void Canva_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = FindLocalPoint(e.Location);
+        }
+
+        private void Canva_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(moving.X == 0)
+            {
+                if (moving.Y == 0)
+                {
+                    start = e.Location;
+                }
+                else if (moving.Y == 1)
+                {
+                    end = e.Location;
+                }
+                Canva.Invalidate();
+            }
+
+            else if (moving.X == 1)
+            {
+                P[moving.Y] = e.Location;
+                Canva.Invalidate();
+            }
+        }
+
+        private void Canva_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = new Point(-1, -1);
+        }
+
+    }
+}
